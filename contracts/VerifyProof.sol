9f05a6ccdf69d55d28427e9dedd36a05496e0dc7
bunoi <taobunoi@gmail.com>
2018-10-18 14:14:40 +0700

    Update new value and return root hash

--- contracts/VerifyProof.sol
+++ contracts/VerifyProof.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 library SMTProofVerifier{
 
-  function verifyProof(bytes32 rootHash, address key, bytes32 value, bytes32[] proof) internal pure returns (bool){
+  function verifyProof(bytes32 rootHash, address key, bytes32 value, bytes32[] proof) internal pure returns(bool) {
     bytes32 currentLeaf = value;
     bytes32 anotherLeaf;
 
@@ -28,6 +28,36 @@ library SMTProofVerifier{
     }
     require(currentLeaf == rootHash, "Invalid proof");
     return true;
+  }
+
+  function update(bytes32 rootHash, address key, bytes32 oldValue, bytes32 newValue, bytes32[] proof) internal pure returns(bytes32 newRootHash){
+    bytes32 currentLeaf = oldValue;
+    newRootHash = newValue;
+    bytes32 anotherLeaf;
+
+    uint256 proofIndex = 1;
+    uint256 mask = uint256(proof[0]);
+
+    for (uint256 i = 1; i < (1 << 160); i <<= 1)
+    {
+      if((mask & i) > 0) {
+        anotherLeaf = bytes32(0);
+      }
+      else {
+        anotherLeaf = proof[proofIndex];
+        proofIndex++;
+      }
+
+      if ((uint(key) & i) == 0) {
+        currentLeaf = zeroHash(currentLeaf, anotherLeaf);
+        newRootHash = zeroHash(newRootHash, anotherLeaf);
+      }
+      else {
+        currentLeaf = zeroHash(anotherLeaf, currentLeaf);
+        newRootHash = zeroHash(anotherLeaf, newRootHash);
+      }
+    }
+    require(currentLeaf == rootHash, "Invalid proof");
   }
 
   function zeroHash(bytes32 left, bytes32 right) private pure returns(bytes32){
--- contracts/mock/VerifyMock.sol
+++ contracts/mock/VerifyMock.sol
@@ -7,4 +7,8 @@ contract VerifyMock {
   function verify(bytes32 rootHash, address key, bytes32 value, bytes32[] proof) public pure returns(bool){
     return SMTProofVerifier.verifyProof(rootHash, key, value, proof);
   }
+
+  function update(bytes32 rootHash, address key, bytes32 oldValue, bytes32 newValue, bytes32[] proof) public pure returns(bytes32){
+    return SMTProofVerifier.update(rootHash, key, oldValue, newValue, proof);
+  }
 }
