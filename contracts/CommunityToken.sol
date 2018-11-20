674c9b76f1192751cec1b2bc78d7b38b777e3dde
Sorawit Suriyakarn <thepsint@gmail.com>
2018-11-20 15:13:21 +0700

    Make Community Token import IERC instead of ERC

--- contracts/CommunityToken.sol
+++ contracts/CommunityToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 
 /**
