fe56d7dd16d65e8baf12c78232bb959108a550ab
prin-r <lang_zane@hotmail.com>
2019-04-30 20:29:18 +0700

    -

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -46,7 +46,7 @@ function* baseInitialize() {
   window.BandWallet = new BandWallet(
     process.env.NODE_ENV === 'production'
       ? 'https://wallet.bandprotocol.com'
-      : 'https://wallet.bandprotocol.com',
+      : 'http://localhost:3001',
     {
       walletPosition: {
         top: 80,
