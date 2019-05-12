5059ac81fb40aa478c49a03302682535b5c33cf0
prin-r <lang_zane@hotmail.com>
2019-05-12 10:56:20 +0700

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -220,7 +220,6 @@ const Navbar = props => {
   const [showNav, setShowNav] = useState(true)
 
   const scrollHistory = useRef()
-  const prevLocation = useRef()
 
   const handleScroll = useCallback(
     e => {
@@ -252,14 +251,10 @@ const Navbar = props => {
   )
 
   useEffect(() => {
-    if (props.location !== prevLocation.current) {
-      setShowMenu(false)
-      setSelectedTab(-1)
-    }
     window.document.body.addEventListener('scroll', handleScroll)
     return () =>
       window.document.body.removeEventListener('scroll', handleScroll)
-  })
+  }, [])
 
   const selectTab = tabId => {
     setSelectedTab(tabId)
