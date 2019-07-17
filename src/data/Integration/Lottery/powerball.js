9e6a1dc845d00b144f0efa127a04919aa27c1774
Sorawit Suriyakarn <swit@bandprotocol.com>
2019-07-17 20:50:07 +0700

    More Powerball WIP

--- src/data/Integration/Lottery/powerball.js
+++ src/data/Integration/Lottery/powerball.js
@@ -4,12 +4,12 @@ export default {
     `Pick the query key for data lookup. For instance, key PWB/20190713 for Powerball result for July 13, 2019. Each dataset has its own method to construct a valid key.`,
   ],
   description: [
-    `Write a simple version of the Power ball rewarding contract. We mock the result of Power ball to bytes7(0) at first. We are going to replace the result by getting it from the Lottery community later.`,
+    `Write a simple version of the Power ball rewarding contract. We mock the result of Power ball to bytes6(0) at first. We are going to replace the result with data provided by Band Protocol later.`,
     `Copy-paste QueryInterface to the top of your smart contract. This interface acts as the gateway to access curated data available on Band Protocol securely. Notice that its query function takes bytes and returns bytes32.`,
-    `Instantiate a QueryInterface object with Lottery community contract address at 0x6863019Ec1A5f675ce64699020A338Ee2256B981. Power ball result can be obtained by query with key PWB/20190420. Note Power ball result consist of 7 number, so we represent it by using bytes7.`,
+    `Instantiate a QueryInterface object with Lottery community contract address at 0x9cd0E16C9b950971fa6c0BA37b9d358117F582aE. Power ball result can be obtained by query with key PWB/20190420. Note Power ball result consist of 6 number, so we represent it by using bytes6.`,
   ],
   label: 'lottery',
-  example: `Say you have a simple Power ball rewarding contract. Participant can pay 1 finney to submit his/her guess to the contract before 04/20/2019. Participant can claim reward (all ETH in the contract) if he/she has submitted the right guess. 👇👇👇`,
+  example: `Say you have a simple Power ball rewarding contract. Participant can pay 1 finney to submit their guess to the contract before 04/20/2019. Participant can claim reward (all ETH in the contract) if they submitted the right guess. 👇👇👇`,
   contractName: 'PowerBallContract',
   dataFormat: {
     description: `Each of the first 6 bytes of the output contains a number of a ball. The first 5 bytes are for the white balls, while the 6th byte is the number of the power ball. The 7th byte is the power play multiplier.`,
@@ -18,7 +18,7 @@ export default {
     crypto: {
       header: 'List of Available Pairs',
       description:
-        'The query key is in the form of •PWB/YYYYMMDD where YYYY, MM, and DD is the year, month, and date of the lottery respectively.',
+        'The query key is •PWB/YYYYMMDD where YYYY, MM, and DD is the year, month, and date of the lottery respectively.',
       keys: [],
     },
   },
@@ -33,11 +33,11 @@ pragma solidity 0.5.9;
 
 contract PowerBallContract {
   // keep the record of guesses submitted by each participant
-  mapping (address => bytes7) public guesses;
+  mapping (address => bytes6) public guesses;
 
-  function guess(bytes7 _guess) public payable  {
+  function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
-      require(guesses[msg.sender] == bytes7(0));
+      require(guesses[msg.sender] == bytes6(0));
       // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
@@ -49,7 +49,41 @@ contract PowerBallContract {
 
 
       // Mock PowerBall result
-      bytes7 result = bytes7(0);
+      bytes6 result = bytes6(0);
+      // sender should provide the right guess before getting reward (all ETH in this contract)
+      require(result == guesses[msg.sender]);
+      // give all ETH in this contract to the sender
+      msg.sender.transfer(address(this).balance);
+  }
+}
+`,
+    `
+pragma solidity 0.5.9;
+
+interface QueryInterface {
+function query(bytes calldata input) external payable returns (bytes32);
+function queryPrice() external view returns (uint256);
+}
+
+contract PowerBallContract {
+  // keep the record of guesses submitted by each participant
+  mapping (address => bytes6) public guesses;
+
+  function guess(bytes6 _guess) public payable  {
+      // sender should never submit his/her guess before
+      require(guesses[msg.sender] == bytes6(0));
+      // Anyone can participate util 04/20/2019 @ 12:00am (UTC)
+      require(now <= 1555718400);
+      // sender should pay 1 finney to submit his/her guess
+      require(msg.value == 1 finney);
+      guesses[msg.sender] = _guess;
+  }
+
+  function claimReward() public payable {
+
+
+      // Mock PowerBall result
+      bytes6 result = bytes6(0);
       // sender should provide the right guess before getting reward (all ETH in this contract)
       require(result == guesses[msg.sender]);
       // give all ETH in this contract to the sender
@@ -72,41 +106,7 @@ contract PowerBallContract {
   function guess(bytes7 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes7(0));
-      // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
-      require(now <= 1555718400);
-      // sender should pay 1 finney to submit his/her guess
-      require(msg.value == 1 finney);
-      guesses[msg.sender] = _guess;
-  }
-
-  function claimReward() public payable {
-
-
-      // Mock PowerBall result
-      bytes7 result = bytes7(0);
-      // sender should provide the right guess before getting reward (all ETH in this contract)
-      require(result == guesses[msg.sender]);
-      // give all ETH in this contract to the sender
-      msg.sender.transfer(address(this).balance);
-  }
-}
-`,
-    `
-pragma solidity 0.5.9;
-
-interface QueryInterface {
-function query(bytes calldata input) external payable returns (bytes32);
-function queryPrice() external view returns (uint256);
-}
-
-contract PowerBallContract {
-  // keep the record of guesses submitted by each participant
-  mapping (address => bytes7) public guesses;
-
-  function guess(bytes7 _guess) public payable  {
-      // sender should never submit his/her guess before
-      require(guesses[msg.sender] == bytes7(0));
-      // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can participate util 04/20/2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
