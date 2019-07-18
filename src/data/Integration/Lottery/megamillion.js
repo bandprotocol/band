aa3323b2b070f94d19380c6777a95e9040bcc22f
prin-r <lang_zane@hotmail.com>
2019-07-18 20:37:54 +0700

    fix lottery info

--- src/data/Integration/Lottery/megamillion.js
+++ src/data/Integration/Lottery/megamillion.js
@@ -1,15 +1,15 @@
 export default {
   overview: [
     `You can integrate lottery data to your DApps with 3 simple steps`,
-    `Pick the query key for data lookup. For instance, key •MMN/20190713 for Mega Millions result for July 13, 2019. Each dataset has its own method to construct a valid key.`,
+    `Pick the query key for data lookup. For instance, key •MMN/20190716 for Mega Millions result for July 16, 2019. Each dataset has its own method to construct a valid key.`,
   ],
   description: [
     `Write a simple version of the Mega Millions rewarding contract. We mock the result of Mega Millions to bytes6(0) at first. We are going to replace the result by getting it from the Lottery community later.`,
     `We then define •QueryInterface at the top of the contract. This gives us access trusted data available on Band Protocol. Notice that the •query is a payable function that takes •bytes and returns •bytes32 together with a timestamp an query status.`,
-    `Instantiate a •QueryInterface object with Lottery community contract address at 0x6863019Ec1A5f675ce64699020A338Ee2256B981. Mega Millions result can be obtained by query with key •MMN/20190420. Note Mega Millions result consist of 6 number, so we represent it by using bytes6.`,
+    `Instantiate a •QueryInterface object with Lottery community contract address at 0x6863019Ec1A5f675ce64699020A338Ee2256B981. Mega Millions result can be obtained by query with key •MMN/20190716. Note Mega Millions result consist of 6 number, so we represent it by using bytes6.`,
   ],
   label: 'lottery',
-  example: `Say you have a simple Mega Millions rewarding contract. Participant can pay 1 finney to submit his/her guess to the contract before 04/20/2019. Participant can claim reward (all ETH in the contract) if he/she has submitted the right guess. 👇👇👇`,
+  example: `Say you have a simple Mega Millions rewarding contract. Participant can pay 1 finney to submit his/her guess to the contract before July 16, 2019. Participant can claim reward (all ETH in the contract) if he/she has submitted the right guess. 👇👇👇`,
   contractName: 'MegaMillionContract',
   dataFormat: {
     description: `Each of the first 6 bytes of the output contains a number of a ball. The first 5 bytes are for the white balls, while the 6th byte is the number of the mega ball.`,
@@ -42,7 +42,7 @@ contract MegaMillionContract {
   function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes6(0));
-      // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can particippate util July 16, 2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
@@ -84,7 +84,7 @@ contract MegaMillionContract {
   function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes6(0));
-      // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can particippate util July 16, 2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
@@ -126,7 +126,7 @@ contract MegaMillionContract {
   function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes6(0));
-      // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can particippate util July 16, 2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
@@ -137,7 +137,7 @@ contract MegaMillionContract {
       // Create a QueryInterface pointing to Lottery community contract
       QueryInterface q = QueryInterface(0x9cd0E16C9b950971fa6c0BA37b9d358117F582aE);
       // Get MegaMillion result from Lottery community
-      (bytes32 rawResult,, QueryInterface.QueryStatus status) = q.query.value(q.queryPrice())("MMN/20190420");
+      (bytes32 rawResult,, QueryInterface.QueryStatus status) = q.query.value(q.queryPrice())("MMN/20190716");
       // Query status should be OK to continue
       require(status == QueryInterface.QueryStatus.OK);
       // The real value only contain in first 6 bytes
--- src/data/Integration/Lottery/powerball.js
+++ src/data/Integration/Lottery/powerball.js
@@ -1,15 +1,15 @@
 export default {
   overview: [
     `You can integrate lottery data to your DApps with 3 steps`,
-    `Pick the query key for data lookup. For instance, key •PWB/20190713 for Powerball result for July 13, 2019. Each dataset has its own method to construct a valid key.`,
+    `Pick the query key for data lookup. For instance, key •PWB/20190717 for Powerball result for July 17, 2019. Each dataset has its own method to construct a valid key.`,
   ],
   description: [
     `Write a simple version of the Power ball rewarding contract. We mock the result of Power ball to bytes6(0) at first. We are going to replace the result with data provided by Band Protocol later.`,
     `We then define •QueryInterface at the top of the contract. This gives us access trusted data available on Band Protocol. Notice that the •query is a payable function that takes •bytes and returns •bytes32 together with a timestamp an query status.`,
-    `Instantiate a •QueryInterface object with Lottery community contract address at 0x9cd0E16C9b950971fa6c0BA37b9d358117F582aE. Power ball result can be obtained by query with key •PWB/20190420. Note Power ball result consist of 6 number, so we represent it by using bytes6.`,
+    `Instantiate a •QueryInterface object with Lottery community contract address at 0x9cd0E16C9b950971fa6c0BA37b9d358117F582aE. Power ball result can be obtained by query with key •PWB/20190717 . Note Power ball result consist of 6 number, so we represent it by using bytes6.`,
   ],
   label: 'lottery',
-  example: `Say you have a simple Power ball rewarding contract. Participant can pay 1 finney to submit their guess to the contract before 04/20/2019. Participant can claim reward (all ETH in the contract) if they submitted the right guess. 👇👇👇`,
+  example: `Say you have a simple Power ball rewarding contract. Participant can pay 1 finney to submit their guess to the contract before July 17, 2019. Participant can claim reward (all ETH in the contract) if they submitted the right guess. 👇👇👇`,
   contractName: 'PowerBallContract',
   dataFormat: {
     description: `Each of the first 6 bytes of the output contains a number of a ball. The first 5 bytes are for the white balls, while the 6th byte is the number of the power ball.`,
@@ -42,7 +42,7 @@ contract PowerBallContract {
   function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes6(0));
-      // Anyone can particippate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can particippate util July 17, 2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
@@ -84,7 +84,7 @@ contract PowerBallContract {
   function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes6(0));
-      // Anyone can participate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can participate util July 17, 2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
@@ -126,7 +126,7 @@ contract PowerBallContract {
   function guess(bytes6 _guess) public payable  {
       // sender should never submit his/her guess before
       require(guesses[msg.sender] == bytes6(0));
-      // Anyone can participate util 04/20/2019 @ 12:00am (UTC)
+      // Anyone can participate util July 17, 2019 @ 12:00am (UTC)
       require(now <= 1555718400);
       // sender should pay 1 finney to submit his/her guess
       require(msg.value == 1 finney);
@@ -137,7 +137,7 @@ contract PowerBallContract {
     // Create a QueryInterface pointing to Lottery community contract
     QueryInterface q = QueryInterface(0x9cd0E16C9b950971fa6c0BA37b9d358117F582aE);
     // Get PowerBall result from Lottery community
-    (bytes32 rawResult,, QueryInterface.QueryStatus status) = q.query.value(q.queryPrice())("PWB/20190420");
+    (bytes32 rawResult,, QueryInterface.QueryStatus status) = q.query.value(q.queryPrice())("PWB/20190717");
     // Query status should be OK to continue
     require(status == QueryInterface.QueryStatus.OK);
     // The real value only contain in first 6 bytes
