e726a661860db1b938f97cbc9a4ce1a1f6f7445f
taobun <taobunoi@gmail.com>
2019-08-19 19:16:25 +0700

    Update truffle-box for band project

---
+++ truffle-box/contracts/GroceryStore.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.5.10;
+
+import "band-solidity/contracts/data/BandLib.sol";
+
+contract GroceryStore is usingBand {
+    using BandLib for Oracle;
+
+    struct Receipt{
+        uint256 priceInUSD;
+        uint256 payEth;
+        uint256 timestamp;
+    }
+
+    mapping(address => Receipt) public lastPay;
+
+    function pay(uint256 priceInUSD) public payable {
+        uint256 ethUsd = CRYPTO.queryUint256("ETH/USD");
+        uint256 price = priceInUSD * 1e36 / ethUsd;
+        require(msg.value >= price);
+        lastPay[msg.sender] = Receipt(priceInUSD, msg.value, now);
+    }
+
+}
