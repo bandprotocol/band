9a87982a0a4be28f6ce5272e63673b3aec96746e
Paul Chonpimai <pchonpimai@gmail.com>
2019-08-20 20:58:23 +0700

    Fix incorrect box size

--- web/src/pages/Feature/TCD.js
+++ web/src/pages/Feature/TCD.js
@@ -236,7 +236,8 @@ const TCDContainer = styled(Flex)`
   background: #ffffff;
   box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.3);
   border-radius: 8px;
-  max-width: 1400px;
+  max-width: 1200px;
+  margin: 0 auto;
   flex-direction: column;
 `
 
