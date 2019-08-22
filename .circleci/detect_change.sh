00d2fc46cdc778a79f45abb2d3a3bc012d75374e
yeast72 <yeast72@gmail.com>
2019-08-22 14:52:30 +0700

    test app build

--- .circleci/detect_change.sh
+++ .circleci/detect_change.sh
@@ -14,14 +14,7 @@
 
             echo -e "LAST_SUCCESSFUL_BUILD_URL $LAST_SUCCESSFUL_BUILD_URL"
             echo -e "LAST_SUCCESSFUL_COMMIT $LAST_SUCCESSFUL_COMMIT"
-            echo -e "Commits $COMMITS"
-            echo -e "$CIRCLE_TOKEN"
-
-            # DIFF=`git diff --no-commit-id --name-only master | cut -d/ -f1 | sort -u`
-
-            # echo -e "git diff file $DIFF"
-
-            # git diff --no-commit-id --name-only master | cut -d/ -f1 | sort -u > projects
+            
             git diff --name-only $COMMITS | cut -d/ -f1 | sort -u > projects
             echo -e "Modified directories:\n`cat projects`\n"
            
--- app/src/App.js
+++ app/src/App.js
@@ -11,6 +11,7 @@ import Routes from 'Routes'
 import FullLoadingPage from 'pages/FullLoading'
 import { fetchSelector } from 'selectors/basic'
 
+//ff
 class App extends Component {
   render() {
     return (
