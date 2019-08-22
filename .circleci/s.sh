c01ae88132f7b9290dde1e56b7a2386e7d1eacad
yeast72 <yeast72@gmail.com>
2019-08-22 19:23:07 +0700

    test app1 change

--- .circleci/s.sh
+++ .circleci/s.sh
@@ -1,3 +1,5 @@
+git diff --name-only $(git merge-base --fork-point master) | cut -d/ -f1 | sort -u
+
 CIRCLE_BRANCH=master
 build=1
 if [[ ${CIRCLE_BRANCH} == "master" ]]; then
@@ -5,18 +7,7 @@ if [[ ${CIRCLE_BRANCH} == "master" ]]; then
   fi
 echo "${build}"
 
-if [[ ${build} == 0]]; then
+if [[ ${build} == "0" ]]; then
   echo "MASTER NOT BUILD"
   exit 0
-  fi
-
-
-  
-
-#first commit in a branch
-if [[ ${LAST_SUCCESSFUL_COMMIT} == "null" ]]; then
-              COMMITS="origin/master"
-            else
-              COMMITS="${CIRCLE_SHA1}..${LAST_SUCCESSFUL_COMMIT}"
-              # COMMITS="master..${LAST_SUCCESSFUL_COMMIT}"
-            fi
+fi
--- app/src/App.js
+++ app/src/App.js
@@ -11,7 +11,7 @@ import Routes from 'Routes'
 import FullLoadingPage from 'pages/FullLoading'
 import { fetchSelector } from 'selectors/basic'
 
-//
+//ss
 
 class App extends Component {
   render() {
