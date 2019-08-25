c1c88efc0981eec05bcc48d2362d0a154c81a99a
Sorawit Suriyakarn <swit@bandprotocol.com>
2019-08-25 23:12:33 +0700

    Add VerifyMessage template function

---
+++ go/eth/util.go
@@ -0,0 +1,7 @@
+package eth
+
+// VerifyMessage checks if the signature is consistent with the message.
+func VerifyMessage(message []byte, sig Signature) bool {
+	// TODO(prin-r): Implement this
+	panic("SignMessage: not implemented")
+}
