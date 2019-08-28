a28f0fa4f53d6a321ad40f129392b62b7e67b4ff
taobun <taobunoi@gmail.com>
2019-08-28 18:20:41 +0700

    Add cache for CoinMarketCap source

--- go/adapter/coinmarketcap.go
+++ go/adapter/coinmarketcap.go
@@ -6,18 +6,29 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/viper"
 	"github.com/tidwall/gjson"
 )
 
-var coinMarketCapApikey = os.Getenv("X-CMC_PRO_API_KEY")
+var coinMarketCapApikey = os.Getenv("CoinMarketCapApikey")
+var coinMarketCapCache = make(map[string]float64)
+var coinMarketCapLastUpdated int64
+var symbols string
 
 type CoinMarketCap struct{}
 
 func (*CoinMarketCap) Configure(config *viper.Viper) {
-	coinMarketCapApikey = config.GetString("apikey")
+	if config.GetString("apikey") != "" {
+		coinMarketCapApikey = config.GetString("apikey")
+	}
+	tokenList := []string{"BCH", "BTC", "ETH", "LTC", "XRP", "BAT", "DAI", "KNC", "LINK", "MKR", "OMG", "REP", "USDC", "ZRX"}
+	for _, token := range tokenList {
+		coinMarketCapCache[token] = 0
+	}
+	symbols = strings.Join(tokenList, ",")
 }
 
 func (*CoinMarketCap) QuerySpotPrice(symbol string) (float64, error) {
@@ -26,39 +37,61 @@ func (*CoinMarketCap) QuerySpotPrice(symbol string) (float64, error) {
 		return 0, fmt.Errorf("spotpx: symbol %s is not valid", symbol)
 	}
 
-	var url strings.Builder
-	url.WriteString("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=")
-	url.WriteString(strings.ToUpper(pairs[0]))
-	url.WriteString("&convert=")
-	url.WriteString(strings.ToUpper(pairs[1]))
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url.String(), nil)
-	if err != nil {
-		return 0, err
-	}
-	req.Header.Add("X-CMC_PRO_API_KEY", coinMarketCapApikey)
-	res, err := client.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return 0, err
+	if _, ok := coinMarketCapCache[pairs[0]]; !ok {
+		return 0, fmt.Errorf("spotpx: symbol %s[%s] is not supported", symbol, pairs[0])
 	}
 
-	result := gjson.GetManyBytes(body, fmt.Sprintf("data.%s.quote.%s.price", pairs[0], pairs[1]), "status.error_code", "status.error_message")
+	if _, ok := coinMarketCapCache[pairs[1]]; !ok && pairs[1] != "USD" {
+		return 0, fmt.Errorf("spotpx: symbol %s[%s] is not supported", symbol, pairs[1])
+	}
 
-	if !result[0].Exists() {
-		if result[1].Exists() {
-			return 0, fmt.Errorf(result[2].String())
+	if time.Now().Unix()-coinMarketCapLastUpdated >= 300 {
+		var url strings.Builder
+		url.WriteString("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=")
+		url.WriteString(symbols)
+		url.WriteString("&convert=USD")
+
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", url.String(), nil)
+		if err != nil {
+			return 0, err
 		}
-		return 0, fmt.Errorf("key does not exist")
+		req.Header.Add("X-CMC_PRO_API_KEY", coinMarketCapApikey)
+		res, err := client.Do(req)
+		if err != nil {
+			return 0, err
+		}
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return 0, err
+		}
+
+		tokenList := strings.Split(symbols, ",")
+		keys := []string{"status.error_code", "status.error_message"}
+		for _, token := range tokenList {
+			keys = append(keys, fmt.Sprintf("data.%s.quote.USD.price", token))
+		}
+
+		result := gjson.GetManyBytes(body, keys...)
+
+		if result[0].Exists() && result[0].Int() != 0 {
+			return 0, fmt.Errorf("%s", result[1].String())
+		}
+
+		for i, token := range tokenList {
+			coinMarketCapCache[token] = result[i+2].Float()
+		}
+		coinMarketCapLastUpdated = time.Now().Unix()
 	}
 
-	return result[0].Float(), nil
+	divisor := 1.0
+	if pairs[1] != "USD" {
+		divisor = coinMarketCapCache[pairs[1]]
+	}
+
+	return coinMarketCapCache[pairs[0]] / divisor, nil
 }
 
 func (a *CoinMarketCap) Query(key []byte) (common.Hash, error) {
