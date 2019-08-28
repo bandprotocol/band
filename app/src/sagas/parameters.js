6063af5ab5ccb6b0d290955d90ab92262245786f
yeast72 <yeast72@gmail.com>
2019-08-28 10:30:26 +0700

    fix parameters web req oracle

--- app/src/sagas/parameters.js
+++ app/src/sagas/parameters.js
@@ -6,6 +6,8 @@ import { Utils } from 'band.js'
 
 import BN from 'utils/bignumber'
 
+const WEB_REQ_ADDRESS = '0x3DEb207E098F882C3F351C494b26B26548a33f5B'
+
 function* handleLoadParameters({ address }) {
   const { currentParameters } = (yield Utils.graphqlRequest(`{
     tokenByAddress(address: "${address}") {
@@ -15,8 +17,13 @@ function* handleLoadParameters({ address }) {
     }
   }`)).tokenByAddress.parameterByTokenAddress
 
+  let listInParams = ['bonding', 'params', 'tcd']
+  if (address === WEB_REQ_ADDRESS) {
+    listInParams = ['bonding', 'params', 'web']
+  }
+
   const params = {}
-  const listInParams = ['bonding', 'params', 'tcd']
+
   for (const [key, value] of Object.entries(currentParameters)) {
     const [prefix, name] = key.split(':')
     if (listInParams.includes(prefix)) {
