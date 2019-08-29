0facdaa4a8bd749ee197abf1ccd56610fef30509
prin-r <q@bandprotocol.com>
2019-08-29 11:28:42 +0700

    add debug log for provider node

--- go/adapter/aggmedian.go
+++ go/adapter/aggmedian.go
@@ -2,8 +2,13 @@ package adapter
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"math/big"
+	"os"
+	"reflect"
 	"sort"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/viper"
@@ -11,6 +16,24 @@ import (
 
 type AggMedian struct {
 	children []Adapter
+}
+
+type AdapterReportInfo struct {
+	AdapterName string
+	Error       error
+	ReportData  common.Hash
+}
+
+var debug bool
+
+func init() {
+	debug = strings.ToLower(os.Getenv("DEBUG")) == "true"
+}
+
+func logDebug(s string) {
+	if debug {
+		log.Println(s)
+	}
 }
 
 func (adpt *AggMedian) Configure(config *viper.Viper) {
@@ -33,23 +56,34 @@ func Median(values []*big.Int) *big.Int {
 }
 
 func (agg *AggMedian) Query(key []byte) (common.Hash, error) {
-	ch := make(chan common.Hash)
+	ch := make(chan AdapterReportInfo)
 	for _, child := range agg.children {
 		go func(child Adapter) {
-			val, err := child.Query(key)
-			if err == nil {
-				ch <- val
+			ari := AdapterReportInfo{}
+			if t := reflect.TypeOf(child); t.Kind() == reflect.Ptr {
+				ari.AdapterName = t.Elem().Name()
 			} else {
-				ch <- common.Hash{}
+				ari.AdapterName = t.Name()
 			}
+			val, err := child.Query(key)
+			ari.Error = err
+			ari.ReportData = val
+			ch <- ari
 		}(child)
 	}
 
 	var values []*big.Int
 	for i := 0; i < len(agg.children); i++ {
 		r := <-ch
-		if r != (common.Hash{}) {
-			values = append(values, r.Big())
+		if r.Error == nil {
+			values = append(values, r.ReportData.Big())
+			logDebug(
+				fmt.Sprintf("Market:%s, ReportData:%s", r.AdapterName, r.ReportData.Big().String()),
+			)
+		} else {
+			logDebug(
+				fmt.Sprintf("Market:%s, Error:%s", r.AdapterName, r.Error),
+			)
 		}
 	}
 
