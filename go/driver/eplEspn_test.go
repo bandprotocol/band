e8a6288535d7762efcab294239e3fafe1dbc627d
yeast72 <yeast72@gmail.com>
2019-09-02 19:59:23 +0700

    done esl

---
+++ go/driver/eplEspn_test.go
@@ -0,0 +1,37 @@
+package driver
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestEplEspnSuccess(t *testing.T) {
+	resolver := &EplEspn{}
+	score, err := resolver.QueryEplScore("20190901", "TOT-ARS")
+	if err != nil {
+		t.Errorf("Query ARS-TOT error: %s", err)
+	}
+	if score[0] != 2 && score[1] != 2 {
+		t.Errorf("Wrong result")
+	}
+}
+
+func TestEplEspnFail(t *testing.T) {
+	resolver := &EplEspn{}
+	_, err := resolver.QueryEplScore("20190901", "ARS-TOT")
+	if err == nil {
+		t.Errorf("Query ARS-TOT error: %s", err)
+	}
+}
+
+func TestEqlEspnSuccessHash(t *testing.T) {
+	resolver := &EplEspn{}
+	hash, err := resolver.Query([]byte("EPL/20190901/TOT-ARS"))
+	if err != nil {
+		t.Errorf("Query EPL error: %s", err)
+	}
+	fmt.Println(hash.Hex())
+	if hash.Hex() != "0x0202000000000000000000000000000000000000000000000000000000000000" {
+		t.Errorf("Wrong result")
+	}
+}
