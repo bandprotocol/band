06502fb29d1ef3a0877326558869859de6dca474
taobun <taobunoi@gmail.com>
2019-09-02 17:32:51 +0700

    Rename contract

--- contracts/contracts/utils/EndpointDatabase.sol
+++ contracts/contracts/utils/EndpointDatabase.sol
@@ -2,14 +2,14 @@ pragma solidity 0.5.9;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract ProviderEndpoint is Ownable {
+contract EndpointDatabase is Ownable {
   mapping(address => string) public endpoints;
 
   function setEndpoint(string calldata endpoint) external {
     endpoints[msg.sender] = endpoint;
   }
 
-  function addEndpointByOwner(address provider, string calldata endpoint) external onlyOwner {
+  function setEndpointByOwner(address provider, string calldata endpoint) external onlyOwner {
     endpoints[provider] = endpoint;
   }
 }
