f86b36d3ee1069ed852d4e316e09719819dd6472
kanisorn <kanisorn.peach@gmail.com>
2019-09-06 23:11:55 +0700

    fix image size for mobile

--- web/src/pages/Feature/DualToken.js
+++ web/src/pages/Feature/DualToken.js
@@ -162,10 +162,12 @@ export default () => {
               alignItems="center"
             >
               <Flex justifyContent="center" flex={1} pr={['0px', '80px']}>
-                <Image
-                  src={DataSetTokenSrc}
-                  style={{ maxWidth: _isMobile ? '' : 500 }}
-                />
+                <Box>
+                  <Image
+                    src={DataSetTokenSrc}
+                    style={{ maxWidth: _isMobile ? '' : 500 }}
+                  />
+                </Box>
               </Flex>
               <Flex pt="12px" flex={1}>
                 <Text
@@ -209,10 +211,12 @@ export default () => {
                 </Text>
               </Flex>
               <Flex justifyContent="center" pl="98.7" flex={1}>
-                <Image
-                  src={DataSetTokenSrc2}
-                  style={{ maxWidth: _isMobile ? '' : 460 }}
-                />
+                <Box>
+                  <Image
+                    src={DataSetTokenSrc2}
+                    style={{ maxWidth: _isMobile ? '' : 460 }}
+                  />
+                </Box>
               </Flex>
             </Flex>
 
