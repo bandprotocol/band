d1f53be5d827ab5a63288f787daedf7f20ea8886
prin-r <q@bandprotocol.com>
2019-09-07 13:42:50 +0700

    fix driver webrequest bug

--- go/driver/webrequest_test.go
+++ go/driver/webrequest_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"testing"
 
+	"github.com/bandprotocol/band/go/dt"
 	"github.com/spf13/viper"
 )
 
@@ -14,7 +15,7 @@ func TestTwitterFollower(t *testing.T) {
 		"1220a9ab69e5da8ac5e378796cef1d9cda4f38d9f42e77ef5aebfceeaf33334de0ed62616e6470726f746f636f6c00",
 	)
 	ans := web.Query(decodeKey)
-	if ans.Option != OK {
+	if ans.Option != dt.Answered {
 		t.Errorf("Option should be OK but got %s", ans.Option)
 	}
 	value := ans.Value.Big().Uint64()
@@ -30,7 +31,7 @@ func TestBinanceBalance(t *testing.T) {
 		"1220784c2aa37a5f5ea6465b6c02be1779684681ec1c625ca469e6ba167157f516f7626e62316a78666832673835713376307464713536666e6576783678637874636e6874736d637536346d00",
 	)
 	ans := web.Query(decodeKey)
-	if ans.Option != OK {
+	if ans.Option != dt.Answered {
 		t.Errorf("Option should be OK but got %s", ans.Option)
 	}
 	value := ans.Value.Big()
@@ -47,7 +48,7 @@ func TestGasStation(t *testing.T) {
 		"1220a39f6304fff1d0e09d093fbb52b733a1dc866d451cb5931d422245396e5596dd6661737400",
 	)
 	ans := web.Query(decodeKey)
-	if ans.Option != OK {
+	if ans.Option != dt.Answered {
 		t.Errorf("Option should be OK but got %s", ans.Option)
 	}
 	value := ans.Value.Big()
@@ -64,7 +65,7 @@ func TestTwitterFollowerExceedParams(t *testing.T) {
 		"1220a9ab69e5da8ac5e378796cef1d9cda4f38d9f42e77ef5aebfceeaf33334de0ed62616e6470726f746f636f6c00a2",
 	)
 	ans := web.Query(decodeKey)
-	if ans.Option != NotFound {
+	if ans.Option != dt.NotFound {
 		t.Errorf("Option should be NotFound but got %s", ans.Option)
 	}
 }
@@ -76,7 +77,7 @@ func TestQueryTooShort(t *testing.T) {
 		"1220a9ab69e5da8ac5e378796cef1d9cda4f",
 	)
 	ans := web.Query(decodeKey)
-	if ans.Option != NotFound {
+	if ans.Option != dt.NotFound {
 		t.Errorf("Option should be NotFound but got %s", ans.Option)
 	}
 }
