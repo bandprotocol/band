f321796fe3ee2b91ed97c77b6f9bc291d5fcf94a
Wisarut Boonnumma <yeast72@gmail.com>
2019-09-24 19:19:29 +0700

    fix redirect (#287)
    - redirect to root when tokenAddress is not valid or undefined
    - 404 page

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import { Flex } from 'ui/common'
-
 import ScrollToTop from 'ScrollToTop'
 import TCDRoutes from 'TCDRoutes'
 import { BackdropProvider } from 'context/backdrop'
@@ -12,91 +12,111 @@ import CommunityDetailPage from 'pages/CommunityDetail'
 import CommunityParameterPage from 'pages/CommunityParameter'
 import CommunityProposalPage from 'pages/CommunityProposal'
 import CreateCommunityPage from 'pages/CreateCommunity'
+import { communitySelector } from 'selectors/basic'
+import NotFound from 'pages/NotFound'
 
-export default ({ match, location }) => (
-  <React.Fragment>
-    <ScrollToTop />
-    <Switch location={location}>
-      <BackdropProvider>
+const Routes = ({ location, communities }) => {
+  return (
+    <React.Fragment>
+      <ScrollToTop />
+      <Switch location={location}>
         <Route
           path="/community/:community/"
-          render={({ match, history, location }) => (
-            <React.Fragment>
-              <Flex
-                flexDirection="row"
-                style={{
-                  minHeight: '100vh',
-                }}
-              >
-                <Sidebar tokenAddress={match.params.community} />
-                <Backdrop />
-                <Switch>
-                  {/* Global Routes */}
-                  <Route
-                    path="/community/:community/overview"
-                    render={({ match }) => (
-                      <CommunityDetailPage
-                        tokenAddress={match.params.community}
-                      />
-                    )}
-                  />
-                  <Route
-                    path="/community/:community/parameters"
-                    render={({ match, location }) => (
-                      <CommunityParameterPage
-                        tokenAddress={match.params.community}
-                        qs={location.search}
-                      />
-                    )}
-                  />
-                  <Route
-                    path="/community/:community/proposal"
-                    render={({ match }) => (
-                      <CommunityProposalPage
-                        tokenAddress={match.params.community}
-                      />
-                    )}
-                  />
+          render={({ match }) => {
+            const communityAddr = match.params.community
+            if (!communityAddr) return <Redirect to="/"></Redirect>
 
-                  {/* TCD Routes */}
-                  <Route
-                    path="/community/:community/:path"
-                    render={({ match }) => (
-                      <TCDRoutes
-                        communityAddress={match.params.community}
-                        path={match.params.path}
-                      />
-                    )}
-                  />
+            const foundCommunityAddress = communities.find(
+              community => community === communityAddr,
+            )
+            if (!foundCommunityAddress) {
+              return <Redirect to="/"></Redirect>
+            }
 
-                  {/* Default TCD Route */}
-                  {/* <Route
-                    path="/community/:community/"
-                    render={({ match }) => (
-                      <Redirect
-                        to={`/community/${match.params.community}/${match.params.tcd}/dataset`}
-                      />
-                    )}
-                  /> */}
-                  {/* Default Global Route */}
-                  <Route
-                    path="/community/:community"
-                    exact
-                    render={({ match }) => (
-                      <Redirect
-                        to={`/community/${match.params.community}/overview`}
-                      />
-                    )}
-                  />
-                </Switch>
-              </Flex>
-            </React.Fragment>
-          )}
+            return (
+              <React.Fragment>
+                <Flex
+                  flexDirection="row"
+                  style={{
+                    minHeight: '100vh',
+                  }}
+                >
+                  <Sidebar tokenAddress={match.params.community} />
+                  <Backdrop />
+                  <Switch>
+                    {/* Global Routes */}
+                    <Route
+                      path="/community/:community/overview"
+                      render={({ match }) => (
+                        <CommunityDetailPage
+                          tokenAddress={match.params.community}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/community/:community/parameters"
+                      render={({ match, location }) => (
+                        <CommunityParameterPage
+                          tokenAddress={match.params.community}
+                          qs={location.search}
+                        />
+                      )}
+                    />
+                    <Route
+                      path="/community/:community/proposal"
+                      render={({ match }) => (
+                        <CommunityProposalPage
+                          tokenAddress={match.params.community}
+                        />
+                      )}
+                    />
+
+                    {/* TCD Routes */}
+                    <Route
+                      path="/community/:community/:path"
+                      render={({ match }) => (
+                        <TCDRoutes
+                          communityAddress={match.params.community}
+                          path={match.params.path}
+                        />
+                      )}
+                    />
+
+                    {/* Default Global Route */}
+                    <Route
+                      path="/community/:community"
+                      exact
+                      render={({ match }) => (
+                        <Redirect
+                          to={`/community/${match.params.community}/overview`}
+                        />
+                      )}
+                    />
+                  </Switch>
+                </Flex>
+              </React.Fragment>
+            )
+          }}
         />
+        {/* Redirect if there is no community address */}
+        <Route path="/community" exact render={() => <Redirect to="/" />} />
 
-        <Route exact path="/create-community" component={CreateCommunityPage} />
+        {/* <Route
+            exact
+            path="/create-community"
+            component={CreateCommunityPage}
+          /> */}
         <Route exact path="/" component={CommunitiesPage} />
-      </BackdropProvider>
-    </Switch>
-  </React.Fragment>
-)
+        {/* Redirect if there is no path */}
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
+  )
+}
+
+const mapStateToProps = state => {
+  const communities = communitySelector(state)
+  return { communities: Object.keys(communities.toJS()) }
+}
+
+export default withRouter(connect(mapStateToProps)(Routes))
