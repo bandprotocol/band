63dd887b61309b31e6fae8a7cc37eda0cf08dbf5
prin-r <q@bandprotocol.com>
2019-09-25 19:40:14 +0700

    connect with smartcontract

--- app/src/actions/transaction.js
+++ app/src/actions/transaction.js
@@ -10,6 +10,7 @@ export const TCD_WITHDRAW = 'TCD_WITHDRAW'
 export const TCD_REVENUE_TO_STAKE = 'TCD_REVENUE_TO_STAKE'
 export const ADD_PENDING_TX = 'ADD_PENDING_TX'
 export const REMOVE_PENDING_TX = 'REMOVE_PENDING_TX'
+export const CLAIM_XFN = 'CLAIM_XFN'
 
 export const addTx = (txHash, title, txType, network) => ({
   type: ADD_TX,
@@ -97,3 +98,10 @@ export const removePendingTx = id => ({
   type: REMOVE_PENDING_TX,
   id,
 })
+
+export const claimXFN = xfnRewardContractAddr => {
+  return {
+    type: CLAIM_XFN,
+    xfnRewardContractAddr,
+  }
+}
--- app/src/components/ClaimXFNModal/index.js
+++ app/src/components/ClaimXFNModal/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ClaimXFNModalRender from './ClaimXFNModalRender'
-import { hideModal } from 'actions'
+import { hideModal, claimXFN } from 'actions'
+import { dispatchAsync } from 'utils/reduxSaga'
 import { walletSelector } from 'selectors/wallet'
+import {
+  xfnRewardInfoSelector,
+  currentNetworkSelector,
+} from 'selectors/current'
+import xfnRewardContracts from 'utils/xfnRewardContracts'
 
 class ClaimXFNModal extends React.Component {
   getXFNAmount = async () => {
@@ -25,13 +31,25 @@ class ClaimXFNModal extends React.Component {
 }
 
 const mapStateToProps = (state, props) => {
+  const xfnRewardInfo = xfnRewardInfoSelector(state)
+  const currentNetwork = currentNetworkSelector(state)
+  const xfnRewardContract = xfnRewardContracts[currentNetwork]
+  const { hasPendingReward, rewardAmount } = xfnRewardInfo || {
+    hasPendingReward: false,
+    rewardAmount: 0,
+  }
   return {
+    hasPendingReward,
+    rewardAmount,
     wallet: walletSelector(state),
+    xfnRewardContract: xfnRewardContract || '',
   }
 }
 
 const mapDispatchToProps = (dispatch, props) => ({
-  hideLogin: () => dispatch(hideModal()),
+  hideXFNRewardModal: () => dispatch(hideModal()),
+  claimXFNReward: xfnRewardContract =>
+    dispatchAsync(dispatch, claimXFN(xfnRewardContract)),
 })
 
 export default connect(
