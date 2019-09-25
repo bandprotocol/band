c388b2c97da67cd764f457828fd896a73482be1e
Sorawit Suriyakarn <swit@bandprotocol.com>
2019-09-25 17:05:44 +0700

    Removen non-price drivers

--- go/driver/types.go
+++ go/driver/types.go
@@ -25,78 +25,26 @@ func FromConfigIndividual(config *viper.Viper) Driver {
 
 	var adpt Driver
 	switch name {
-	case "RegEx":
-		adpt = &RegEx{}
 	case "AggMedian":
 		adpt = &AggMedian{}
-	case "AggMajority":
-		adpt = &AggMajority{}
-	case "IdleDriver":
-		adpt = &IdleDriver{}
-	case "DelegateDriver":
-		adpt = &DelegateDriver{}
-	case "WebRequest":
-		adpt = &WebRequest{}
-	case "AlphaVantageStock":
-		adpt = &AlphaVantageStock{}
-	case "WorldTradingData":
-		adpt = &WorldTradingData{}
 	case "Bittrex":
 		adpt = &Bittrex{}
-	case "Kyber":
-		adpt = &Kyber{}
 	case "CryptoCompare":
 		adpt = &CryptoCompare{}
-	case "Ratesapi":
-		adpt = &Ratesapi{}
-	case "Bitstamp":
-		adpt = &Bitstamp{}
 	case "Gemini":
 		adpt = &Gemini{}
-	case "FinancialModelPrep":
-		adpt = &FinancialModelPrep{}
 	case "Coinbase":
 		adpt = &Coinbase{}
-	case "OpenMarketCap":
-		adpt = &OpenMarketCap{}
-	case "FreeForexApi":
-		adpt = &FreeForexApi{}
-	case "CurrencyConverter":
-		adpt = &CurrencyConverter{}
 	case "CoinMarketCap":
 		adpt = &CoinMarketCap{}
 	case "Kraken":
 		adpt = &Kraken{}
-	case "OnChainFX":
-		adpt = &OnChainFX{}
 	case "Bitfinex":
 		adpt = &Bitfinex{}
-	case "Uniswap":
-		adpt = &Uniswap{}
-	case "Bancor":
-		adpt = &Bancor{}
-	case "AlphaVantageForex":
-		adpt = &AlphaVantageForex{}
-	case "PriceHttp":
-		adpt = &PriceHttp{}
 	case "CoinGecko":
 		adpt = &CoinGecko{}
 	case "BinanceAmerica":
 		adpt = &BinanceAmerica{}
-	case "Binance":
-		adpt = &Binance{}
-	case "EplEspn":
-		adpt = &EplEspn{}
-	case "EplSport":
-		adpt = &EplSport{}
-	case "NbaEspn":
-		adpt = &NbaEspn{}
-	case "DataNba":
-		adpt = &DataNba{}
-	case "MlbEspn":
-		adpt = &MlbEspn{}
-	case "NflEspn":
-		adpt = &NflEspn{}
 	default:
 		panic(fmt.Sprintf("driver.FromConfig: unknown driver name %s", name))
 	}
