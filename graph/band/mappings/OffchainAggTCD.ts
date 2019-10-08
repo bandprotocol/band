98f5608a0379843d2ad8f154683c5b5849c14511
prin-r <q@bandprotocol.com>
2019-10-08 17:57:58 +0700

    fix link between dataProvider and dataProviderOwnerships

--- graph/band/mappings/OffchainAggTCD.ts
+++ graph/band/mappings/OffchainAggTCD.ts
@@ -30,12 +30,8 @@ function updateProvider(
   let dpoKey = dpKey + "-" + participant.toHexString();
   let voterOwnership = DataProviderOwnership.load(dpoKey);
 
-  let tcdContract = OffchainAggTCD.bind(
-    Address.fromString(tcdAddress.toHexString())
-  );
-  let dataSourceInfo = tcdContract.infoMap(
-    Address.fromString(dataSourceAddress.toHexString())
-  );
+  let tcdContract = OffchainAggTCD.bind(Address.fromString(tcdAddress.toHexString()));
+  let dataSourceInfo = tcdContract.infoMap(Address.fromString(dataSourceAddress.toHexString()));
 
   dataProvider.stake = dataSourceInfo.value1;
   dataProvider.totalOwnership = dataSourceInfo.value2;
@@ -47,21 +43,17 @@ function updateProvider(
 
   if (voterOwnership == null && newVoterOwnership.gt(new BigInt(0))) {
     voterOwnership = new DataProviderOwnership(dpoKey);
-    voterOwnership.providerAddress = Address.fromString(
-      dataSourceAddress.toHexString()
-    );
+    voterOwnership.providerAddress = Address.fromString(dataSourceAddress.toHexString());
     voterOwnership.tcdAddress = Address.fromString(tcdAddress.toHexString());
     voterOwnership.voter = Address.fromString(participant.toHexString());
-    //voterOwnership.tokenLock = ''
+    voterOwnership.dataProvider = dpKey;
     voterOwnership.ownership = newVoterOwnership;
+
     voterOwnership.save();
   } else if (voterOwnership != null && newVoterOwnership.gt(new BigInt(0))) {
     voterOwnership.ownership = newVoterOwnership;
     voterOwnership.save();
-  } else if (
-    voterOwnership != null &&
-    newVoterOwnership.equals(new BigInt(0))
-  ) {
+  } else if (voterOwnership != null && newVoterOwnership.equals(new BigInt(0))) {
     store.remove("DataProviderOwnership", dpoKey);
   }
 
@@ -87,8 +79,7 @@ export function handleDataUpdated(event: DataUpdated): void {
   tcd.reportCount = tcd.reportCount + 1;
   tcd.save();
 
-  let key =
-    event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
+  let key = event.transaction.hash.toHexString() + "-" + event.logIndex.toString();
 
   let report = Report.load(key);
   if (report == null) {
@@ -121,8 +112,7 @@ export function handleDataSourceRegistered(event: DataSourceRegistered): void {
     tcd.save();
   }
 
-  let dPKey =
-    event.params.dataSource.toHexString() + "-" + event.address.toHexString();
+  let dPKey = event.params.dataSource.toHexString() + "-" + event.address.toHexString();
   let dpoKey = dPKey + "-" + event.params.owner.toHexString();
 
   let dataProvider = new DataProvider(dPKey);
@@ -145,24 +135,15 @@ export function handleDataSourceRegistered(event: DataSourceRegistered): void {
 }
 
 export function handleDataSourceStaked(event: DataSourceStaked): void {
-  updateProvider(
-    event.address,
-    event.params.dataSource,
-    event.params.participant
-  );
+  updateProvider(event.address, event.params.dataSource, event.params.participant);
 }
 
 export function handleDataSourceUnstaked(event: DataSourceUnstaked): void {
-  updateProvider(
-    event.address,
-    event.params.dataSource,
-    event.params.participant
-  );
+  updateProvider(event.address, event.params.dataSource, event.params.participant);
 }
 
 export function handleFeeDistributed(event: FeeDistributed): void {
-  let dPKey =
-    event.params.dataSource.toHexString() + "-" + event.address.toHexString();
+  let dPKey = event.params.dataSource.toHexString() + "-" + event.address.toHexString();
   let dataProvider = DataProvider.load(dPKey);
 
   updateProvider(
@@ -172,13 +153,9 @@ export function handleFeeDistributed(event: FeeDistributed): void {
   );
 }
 
-export function handleWithdrawReceiptCreated(
-  event: WithdrawReceiptCreated
-): void {}
+export function handleWithdrawReceiptCreated(event: WithdrawReceiptCreated): void {}
 
-export function handleWithdrawReceiptUnlocked(
-  event: WithdrawReceiptUnlocked
-): void {}
+export function handleWithdrawReceiptUnlocked(event: WithdrawReceiptUnlocked): void {}
 
 export function handleQuery(event: Query): void {
   let tcd = TCD.load(event.address.toHexString());
