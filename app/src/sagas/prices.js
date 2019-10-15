1c934d6f67314f45f74973829245bcceeef477fc
taobun <taobunoi@gmail.com>
2019-10-15 20:13:11 +0700

    Query pricr more than 100 points

--- app/src/sagas/prices.js
+++ app/src/sagas/prices.js
@@ -7,20 +7,29 @@ function* handleLoadPriceHistory(props) {
   if (!props) return
   const { address } = props
   if (!address) return
-  const query = yield Utils.graphqlRequest(`
-  {
-    token(id:"${address}") {
-      curve {
-        prices (orderBy: timestamp, orderDirection: desc) {
-          price
-          timestamp
+
+  let prices = []
+  for (let i = 0; i < 1000; i++) {
+    const query = yield Utils.graphqlRequest(`
+    {
+      token(id:"${address}") {
+        curve {
+          prices (orderBy: timestamp, orderDirection: desc, first: 1000, skip: ${i *
+            1000}) {
+            price
+            timestamp
+          }
         }
       }
     }
+    `)
+    if (query.token.curve.prices.length === 0) {
+      break
+    }
+    prices = prices.concat(query.token.curve.prices)
   }
-  `)
 
-  yield put(addPrices(address, query.token.curve.prices))
+  yield put(addPrices(address, prices))
 }
 
 export default function*() {
