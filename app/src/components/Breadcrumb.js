25b46c2e0c2127f49f003967354c44776f411cad
yeast72 <yeast72@gmail.com>
2019-10-17 17:54:16 +0700

    Breadcrumb

--- app/src/components/Breadcrumb.js
+++ app/src/components/Breadcrumb.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { Text, Flex, Link, Image } from 'ui/common'
 
 import Chevron from 'images/chevron.svg'
+import { FormattedMessage } from 'react-intl'
 
 const BreadcrumbLink = styled(Link)`
   ${p =>
@@ -19,13 +20,17 @@ const BreadcrumbLink = styled(Link)`
 export default ({ links = [] }) => (
   <Flex mt="12px" mb="28px">
     <BreadcrumbLink to="/">
-      <Text fontSize="14px">Governance Portal</Text>
+      <Text fontSize="14px">
+        <FormattedMessage id="Governance Portal"></FormattedMessage>
+      </Text>
     </BreadcrumbLink>
     {links.map(({ path, label }, i) => (
       <React.Fragment key={path}>
         <Image mx={3} src={Chevron} width="6px" />
         <BreadcrumbLink to={path} active={i === links.length - 1 ? 1 : 0}>
-          <Text fontSize="14px">{label}</Text>
+          <Text fontSize="14px">
+            <FormattedMessage id={label}></FormattedMessage>
+          </Text>
         </BreadcrumbLink>
       </React.Fragment>
     ))}
